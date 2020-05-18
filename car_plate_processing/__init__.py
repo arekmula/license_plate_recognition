7cb1dd1edd16c42201691e2b9e7d557f68eeae92
Arkadiusz Mula <arekmula97@gmail.com>
2020-05-18 21:27:28 +0200

    Added project template

---
+++ car_plate_processing/__init__.py
@@ -0,0 +1,1 @@
+from .utils import perform_processing
---
+++ car_plate_processing/utils.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+
+def perform_processing(image: np.ndarray) -> str:
+    print(f'image.shape: {image.shape}')
+    # TODO: add image car_plate_processing here
+    return 'PO12345'
