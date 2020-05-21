8ce5014a7ed9b2a4aa455c0a38413f9f1569c53a
Arkadiusz Mula <arekmula97@gmail.com>
2020-05-22 00:01:23 +0200

    Added warpPerspective
    You can now look at car plate from "bird-eye" view

--- car_plate_processing/utils.py
+++ car_plate_processing/utils.py
@@ -1,13 +1,7 @@
 import numpy as np
 import cv2
-from skimage.color import rgb2gray
-from skimage import data
-from skimage.filters import gaussian
-from skimage.segmentation import active_contour
 from matplotlib import pyplot as plt
 
-
-platesFound = []
 
 def empty_callback(value):
     pass
@@ -16,79 +10,80 @@ def empty_callback(value):
 def perform_processing(image: np.ndarray) -> str:
     # print(f'image.shape: {image.shape}')
 
+    print("\n \n \n \n \n \n")
+
     # TODO: add image car_plate_processing here
     # convert image to gray scale
     gray_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
     # resize image for faster processing
     gray_img = cv2.resize(gray_img, None, fx=0.3, fy=0.3)
+    image_resied = cv2.resize(image, None, fx=0.3, fy=0.3)
 
     # get shape of resized image
     height = gray_img.shape[0]
     width = gray_img.shape[1]
 
     # blur image
-    gray_blur = cv2.GaussianBlur(gray_img, (5, 5), 0)
+    gray_blur = cv2.bilateralFilter(gray_img, 11, 17, 17)
 
-    # thresh image with adaptive gaussian thresholding
-    gray_thresh_adaptive = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 199, 3)
-
-    # show image threshed with adaptive thresholding
-    cv2.imshow('Adaptive', gray_thresh_adaptive)
+    # find edges in image
+    gray_edges = cv2.Canny(gray_blur, 30, 200)
 
     # The size of car_plates in Poland is 520 x 114 mm
     # width to height ratio of car plates in Poland.
     # choose smaller ratio to accept bigger contours
     height_to_width_ratio = 90/520
 
-    # find contours on image threshed by adaptive thresholding
-    contours, hierarchy = cv2.findContours(gray_thresh_adaptive, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # find contours on image with edges
+    contours, hierarchy = cv2.findContours(gray_edges.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-    potential_plates = []
+    # find potential contours that matches car plate dimensions
+    potential_plates_vertices = []
     for contour in contours:
-        # TODO: try diffrent bounding, maybe one to match proper contour of car plate
         [x, y, w, h] = cv2.boundingRect(contour)
 
         # exclude contours that are smaller than 1/3 of image width and their height doesn't match ratio of car_plate
         if w < (width/3) or h < (w*height_to_width_ratio) or w == width:
             continue
 
-        # print(f'w: {w} width/3: {width/3} h: {h}')
+        # reshape contour of potential plate
+        pts = contour.reshape(contour.shape[0],2)
+        # vertices of plate rectangle
+        vertices = np.zeros((4, 2), dtype="float32")
+        # top left point has smallest sum and bottom right has smallest sum
+        s = pts.sum(axis=1)
+        vertices[0] = pts[np.argmin(s)]
+        vertices[2] = pts[np.argmax(s)]
+        # top right has minimum difference and bottom left has maximum difference
+        diff = np.diff(pts, axis=1)
+        vertices[1] = pts[np.argmin(diff)]
+        vertices[3] = pts[np.argmax(diff)]
+        potential_plates_vertices.append(vertices)
 
-        # crop image to consider only potential plates to speed up processing
-        potential_plates.append(gray_thresh_adaptive[y:y + h, x:x + w])
-        # cv2.rectangle(gray_img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-
-    # if no potential plates are found in image threshed by adaptive thresholding, try OTSU threshold
-    if len(potential_plates) == 0:
-        ret, gray_thresh_Otsu = cv2.threshold(gray_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-        contoursOtsu, hierarchyOtsu = cv2.findContours(gray_thresh_Otsu, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        for contour in contoursOtsu:
-            # TODO: try diffrent bounding, maybe one to match proper contour of car plate
-            [x, y, w, h] = cv2.boundingRect(contour)
-
-            # exclude contours that are smaller than 1/3 of image width and their height doesn't match ratio of car_plate
-            if w < (width/3) or h < (w*height_to_width_ratio) or w == width:
-                continue
-
-            # print(f'w: {w} width/3: {width/3} h: {h}')
-            # crop image to consider only potential plates to speed up processing
-            potential_plates.append(gray_thresh_adaptive[y:y + h, x:x + w])
-            # cv2.rectangle(gray_img, (x, y), (x + w, y + h), (255, 0, 255), 2)
-
-    # counter of found plates
-    if len(potential_plates) > 0:
-        platesFound.append(1)
-
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE,(5,5))
-
-    # Show every potential plate found
-    for idx, plate in enumerate(potential_plates):
-        plate_closed = cv2.morphologyEx(plate, cv2.MORPH_CLOSE, kernel)
-        cv2.imshow('plate ' + str(idx), plate_closed)
-
-    # print number of found plates in all images
-    print("Plates found: ", len(platesFound))
+    # change perspective in all potential car plates, to "birds eye" view
+    for idx, vertices in enumerate(potential_plates_vertices):
+        # get all corners in easier way to code
+        (tl, tr, br, bl) = vertices
+        # compute width and height of image created by corners
+        widthA = np.sqrt(((br[0] - bl[0]) ** 2) + ((br[1] - bl[1]) ** 2))
+        widthB = np.sqrt(((tr[0] - tl[0]) ** 2) + ((tr[1] - tl[1]) ** 2))
+        heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
+        heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
+        # take the maximum of the width and height values to reach final dimensions
+        maxWidth = max(int(widthA), int(widthB))
+        maxHeight = max(int(heightA), int(heightB))
+        # construct destination points which will be used to map the screen to a top-down, "birds eye" view
+        dst = np.array([
+            [0, 0],
+            [maxWidth - 1, 0],
+            [maxWidth - 1, maxHeight - 1],
+            [0, maxHeight - 1]], dtype="float32")
+        # calculate the perspective transform matrix and warp the perspective to grab the screen
+        M = cv2.getPerspectiveTransform(vertices, dst)
+        warp = cv2.warpPerspective(gray_edges, M, (maxWidth, maxHeight))
+        # show warped image
+        cv2.imshow('warp' + str(idx), warp)
 
     cv2.waitKey()
     cv2.destroyAllWindows()
